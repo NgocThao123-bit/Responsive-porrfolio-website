c92678c42cbd9087f425dcaf2be4383733ddc5cf
NgocThao123-bit <73955710+NgocThao123-bit@users.noreply.github.com>
2024-09-29 14:23:15 -0400

    Modifying footer ver2

--- portfolio-web-thao-ngo/src/Components/Contact/Contact.jsx
+++ portfolio-web-thao-ngo/src/Components/Contact/Contact.jsx
@@ -3,28 +3,28 @@ import ContactCSS from '../Contact/Contact.module.css'
 
 
 export default function Contact() {
-  return (
-    <>
-    <section id='contact'>
-<div className={ContactCSS.contact}>
-      <div>
-            <h2>Contact Me</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      return (
+            <>
+                  <section id='contact'>
+                        <div className={ContactCSS.contact}>
+                              <div>
+                                    <h2>Contact Me</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
-      </div>
-      <form action="">
-            <label htmlFor="">Name :</label>
-            <input type="text" placeholder='Enter Name *' id="name" required/>
-            <label htmlFor="">Email :</label>
-            <input type="text" placeholder='Enter Email *' id="email" required/>
-            <label htmlFor="">Phone :</label>
-            <input type="text" placeholder='Enter phone *' id="phone" required/>
-            <button>Contact</button>
-      </form>
-      
+                              </div>
+                              <form action="">
+                                    <label htmlFor="">Name :</label>
+                                    <input type="text" placeholder='Enter Name *' id="name" required />
+                                    <label htmlFor="">Email :</label>
+                                    <input type="text" placeholder='Enter Email *' id="email" required />
+                                    <label htmlFor="">Phone :</label>
+                                    <input type="text" placeholder='Enter phone *' id="phone" required />
+                                    <button>Contact</button>
+                              </form>
 
-</div>
-    </section>
-    </>
-  )
+
+                        </div>
+                  </section>
+            </>
+      )
 }
--- portfolio-web-thao-ngo/src/Components/Project/Project.jsx
+++ portfolio-web-thao-ngo/src/Components/Project/Project.jsx
@@ -56,7 +56,7 @@ export default function Project() {
                                                 </a>
                                           </div>
                                     </div>
-                                   
+
                               </div>
                         </div>
                   </section>
